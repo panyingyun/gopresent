766428422ae8a636608bb6a005972dac097e7d6c
Michael_Pan <yypan@yuansuan.cn>
2022-01-09 12:07:20 +0800

    fix no header issue

--- main.go
+++ main.go
@@ -48,6 +48,7 @@ func main() {
 		*usePlayground = true
 		*contentPath = "./content/"
 	}
+	http.HandleFunc("/", dirHandler)
 	err := initTemplates("tpl")
 	if err != nil {
 		log.Fatalf("Failed to parse templates: %v", err)
@@ -94,39 +95,49 @@ func main() {
 	}
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/favicon.ico")
+		w.Header().Set("Content-Type", "image/x-icon")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/styles.css", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/styles.css")
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/dir.css", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/dir.css")
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/jquery-ui.js", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/jquery-ui.js")
+		w.Header().Set("Content-Type", "application/javascript")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/notes.css", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/notes.css")
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/notes.js", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/notes.js")
+		w.Header().Set("Content-Type", "application/javascript")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/slides.js", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/slides.js")
+		w.Header().Set("Content-Type", "application/javascript")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/article.css", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/article.css")
+		w.Header().Set("Content-Type", "text/css; charset=utf-8")
 		w.Write(buffer)
 	})
 	http.HandleFunc("/static/dir.js", func(w http.ResponseWriter, r *http.Request) {
 		buffer, _ := Asset("tpl/static/dir.js")
+		w.Header().Set("Content-Type", "application/javascript")
 		w.Write(buffer)
+
 	})
 
 	if !ln.Addr().(*net.TCPAddr).IP.IsLoopback() &&
